f9b76e103d5e43a7f1a98fc5f471e8e711588edd
FromJ <yuha.j.w@gmail.com>
2025-11-28 09:30:15 +0900

    fix(path): GitHub Pages 배포를 위한 컴포넌트 로딩 경로 수정 5
    fetch() 호출 시 발생한 404 오류를 해결하기 위해 index.html에 base 태그 삽입하여 기점 확보함. common.js에 components 호출은 2가지 경로 모두 작성하고 한쪽 주석처리해 둠. 본 수정본은 Git Pages 배포 가능 버전이나. 개별 브랜치 환경에서 작업하여 생긴 오류일 수 있으므로 이 커밋 적용 전 반드시 1을 적용한 후 적용할 것

--- js/common.js
+++ js/common.js
@@ -25,25 +25,32 @@ async function loadComponent(url, containerId, repeatCount = 1) {
     }
 }
 
-// 컴포넌트 로드
+// 컴포넌트 로드(Git 배포 오류 시 index.html에 base 태그과 함께 활성화)
 const requiredComponents = [
     loadComponent('./components/sidebar.html', 'sidebar-container', 1),
     loadComponent('./components/story-item.html', 'story-item-container', 5),
     loadComponent('./components/post-card.html', 'post-card-container', 3),
     loadComponent('./components/profile-post-card.html', 'profile__feed-item-container', 8),
-    // ▼ GitPage배포를 위해 주석처리
-    // loadComponent('../components/sidebar.html', 'sidebar-container', 1),
-    // loadComponent('../components/story-item.html', 'story-item-container', 5),
-    // loadComponent('../components/post-card.html', 'post-card-container', 3),
-    // loadComponent('../components/profile-post-card.html', 'profile__feed-item-container', 8),
-    
     
     // 추천 사용자 로드 + 내부 아이템 로드 (컴포넌트화가 필요한 줄 알았으나 필요 X -> 추후 수정 예정)
-    loadComponent('../components/recommended-users.html', 'recommended-users-container', 1)
+    loadComponent('./components/recommended-users.html', 'recommended-users-container', 1)
         .then(() => {
             return loadComponent('components/recommended-item.html', 'recommended-users__item-container', 5);
         })
 ];
+// 컴포넌트 로드(Git배포 오류 없을 시 활성화 base 태그 주석처리할 것)
+// const requiredComponents = [
+//     loadComponent('../components/sidebar.html', 'sidebar-container', 1),
+//     loadComponent('../components/story-item.html', 'story-item-container', 5),
+//     loadComponent('../components/post-card.html', 'post-card-container', 3),
+//     loadComponent('../components/profile-post-card.html', 'profile__feed-item-container', 8),
+   
+//     // 추천 사용자 로드 + 내부 아이템 로드 (컴포넌트화가 필요한 줄 알았으나 필요 X -> 추후 수정 예정)
+//     loadComponent('../components/recommended-users.html', 'recommended-users-container', 1)
+//         .then(() => {
+//             return loadComponent('components/recommended-item.html', 'recommended-users__item-container', 5);
+//         })
+// ];
 
 // Promise 사용하여 렌더링 보장(모달이벤트핸들러 등을 위함)
 Promise.all(requiredComponents)
